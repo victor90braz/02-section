a8537daba84b4dc87d1113bd52cd6ba37051b7b7
victor braz <victor.90braz@gmail.com>
2023-10-16 16:41:29 +0200

    create 3 articles

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/app.css">
+    <script src="/js/app.js"></script>
+    <title>Blog</title>
+</head>
+<body>
+    <article class="container">
+
+        <h1>Blog Post</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Aliquam quis animi excepturi nam porro iusto tempore qui minima eaque.
+            At aliquid voluptatum optio dolores rem exercitationem facilis autem, minus sequi.
+        </p>
+
+    </article>
+
+    <article class="container">
+
+        <h1>Second Blog Post</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Aliquam quis animi excepturi nam porro iusto tempore qui minima eaque.
+            At aliquid voluptatum optio dolores rem exercitationem facilis autem, minus sequi.
+        </p>
+
+    </article>
+
+    <article class="container">
+
+        <h1>Third Blog Post</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Aliquam quis animi excepturi nam porro iusto tempore qui minima eaque.
+            At aliquid voluptatum optio dolores rem exercitationem facilis autem, minus sequi.
+        </p>
+
+    </article>
+</body>
+</html>
