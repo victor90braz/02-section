f48f0004ff76de4db707028cd2b68dbe01de7bb3
victor braz <victor.90braz@gmail.com>
2023-10-18 13:46:05 +0200

    ddd($object->matter('title'));

--- routes/web.php
+++ routes/web.php
@@ -2,23 +2,25 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', function () {
+    $object = YamlFrontMatter::parseFile(resource_path("posts/fourthPost.html"));
+    ddd($object->matter('title'));
 
+    /*
     $posts = Post::all();
 
     return view('posts', [
         'posts' => $posts
     ]);
+    */
 
 });
 
 Route::get('posts/{post}', function ($slug) {
-
     $post = Post::find($slug);
-
     return view('post', [
         'post' => $post
     ]);
-
 })->where('post', '[A-Za-z_\-]+');
