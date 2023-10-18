c90463281405f07e41a8425d23816474c002a929
victor braz <victor.90braz@gmail.com>
2023-10-18 15:09:07 +0200

    UPDATED REFACTORED

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -10,15 +10,10 @@
 </head>
 <body>
     <div class="container">
-
         <article>
-
             <h1><?= $post->title ?></h1>
-
             <div><?= $post->body ?></div>
-
         </article>
-
         <a href="/">Go Back</a>
     </div>
 </body>
