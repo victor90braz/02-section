c6955166a610cde71d296c908895f956ff87e134
victor braz <victor.90braz@gmail.com>
2023-10-18 15:32:37 +0200

    php artisan thinker

--- README.md
+++ README.md
@@ -9,3 +9,31 @@
 # spatie/yaml-front-matter
 
 composer require spatie/yaml-front-matter
+
+# cache()->rememberForever
+
+    public static function all() {
+
+        return cache()->rememberForever('posts.all', function () {
+            $files = File::files(resource_path("posts"));
+
+            return collect($files)->map(function ($file) {
+                $document = YamlFrontMatter::parseFile($file);
+
+                return new Post(
+                    $document->title,
+                    $document->excerpt,
+                    $document->date,
+                    $document->body(),
+                    $document->slug
+                );
+            })->sortByDesc('date');
+        });
+    }
+
+# run terminal
+
+php artisan thinker
+
+-   yes
+-   cache('posts.all')
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -25,18 +25,21 @@ class Post {
     }
 
     public static function all() {
-        $files = File::files(resource_path("posts"));
 
-        return collect($files)->map(function ($file) {
-            $document = YamlFrontMatter::parseFile($file);
+        return cache()->rememberForever('posts.all', function () {
+            $files = File::files(resource_path("posts"));
 
-            return new Post(
-                $document->title,
-                $document->excerpt,
-                $document->date,
-                $document->body(),
-                $document->slug
-            );
-        })->sortByDesc('date');
+            return collect($files)->map(function ($file) {
+                $document = YamlFrontMatter::parseFile($file);
+
+                return new Post(
+                    $document->title,
+                    $document->excerpt,
+                    $document->date,
+                    $document->body(),
+                    $document->slug
+                );
+            })->sortByDesc('date');
+        });
     }
 }
