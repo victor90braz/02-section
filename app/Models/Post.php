65b2c9aac182647e30a6e6e272d33627005952dd
victor braz <victor.90braz@gmail.com>
2023-10-18 15:18:20 +0200

    sortByDesc('date');

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -24,21 +24,19 @@ class Post {
         return static::all()->firstWhere('slug', $slug);
     }
 
-public static function all() {
-    $files = File::files(resource_path("posts"));
+    public static function all() {
+        $files = File::files(resource_path("posts"));
 
-    return collect($files)->map(function ($file) {
-        $document = YamlFrontMatter::parseFile($file);
+        return collect($files)->map(function ($file) {
+            $document = YamlFrontMatter::parseFile($file);
 
-        return new Post(
-            $document->title,
-            $document->excerpt,
-            $document->date,
-            $document->body(),
-            $document->slug
-        );
-    })->sortBy('date');
-}
-
-
+            return new Post(
+                $document->title,
+                $document->excerpt,
+                $document->date,
+                $document->body(),
+                $document->slug
+            );
+        })->sortByDesc('date');
+    }
 }
