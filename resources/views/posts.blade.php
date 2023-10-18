334e3972a1c3ece75e933b460f0b22241ae0635e
victor braz <victor.90braz@gmail.com>
2023-10-18 14:31:28 +0200

    __construct($title, $excerpt, $date, $body, $slug)

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -12,7 +12,11 @@
     <div class="container">
         <?php foreach ($posts as $post) : ?>
             <article>
-                <h1><?= $post->title ?></h1>
+                <h1>
+                    <a href="/posts/<?= $post->slug ?>">
+                        <?= $post->title ?>
+                    </a>
+                </h1>
 
                 <div>
                     <?= $post->body ?>
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
             $document->excerpt,
             $document->date,
             $document->body(),
+            $document->slug,
         );
     }
 
